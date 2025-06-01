eac77fe7d7c8ce55925c8f5e7a53bffd3c668fd0
Gabriel Herling <gabo.gh12398@gmail.com>
2025-06-01 12:36:56 -0300

    Tienda Online Básica V1.3

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function addToCart(nombre, precio) {
-            alert(`Agregaste "${nombre}" al carrito. Precio: $${precio}`);
-        }
+let elementoPadre = '';
+let nombreProducto = '';
+let precioProducto = '';
+const btn = document.querySelectorAll('.btn');
+
+const agregarCarrito = (nombre, precio) => {
+    alert(`Agregaste "${nombre}" al carrito. Precio: ${precio}`);
+}
+
+for (let i of btn) {
+    i.addEventListener('click', e => {
+        elementoPadre = e.target.parentElement;
+        nombreProducto = elementoPadre.querySelector('.nombre').textContent;
+        precioProducto = elementoPadre.querySelector('.precio').textContent;
+
+        agregarCarrito(nombreProducto, precioProducto);
+    })
+}
